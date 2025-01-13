2ba232b15920f4edde22494d9b841780e3a959ec
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2025-01-13 21:04:46 +0300

    small fix

--- src/features/badge/closePayment.tsx
+++ src/features/badge/closePayment.tsx
@@ -3,22 +3,24 @@ import { Icon } from "@/shared/ui/icon/ui/icon";
 import Modal from "@/shared/ui/modal/modal";
 import ModalWrapper from "@/widgets/modalWrapper/ui/modalWrapper";
 
-const ClosePayment = () => (
-  <ModalWrapper className="-translate-y-52">
-    <Modal isOpen>
-      <div className="text-white bg-dark-200 rounded-xl flex justify-center flex-col items-center space-y-3 p-5 py-8 sm:w-[479px]">
-        <span>
-          <Icon type={IconTypes.CLOSE_CIRCLE_OUTLINED} />
-        </span>
-        <h1 className="text-[24px] sm:text-[40px] font-bold text-center sm:leading-[45px]">
-          Время на оплату счета истекло
-        </h1>
-        <p className="sm:text-xl text-center">
-          Запросите новую ссылку для оплаты
-        </p>
-      </div>
-    </Modal>
-  </ModalWrapper>
-);
+const ClosePayment = () => {
+  return (
+    <ModalWrapper className="-translate-y-52">
+      <Modal isOpen>
+        <div className="text-white bg-dark-200 rounded-xl flex justify-center flex-col items-center space-y-3 p-5 py-8 sm:w-[479px]">
+          <span>
+            <Icon type={IconTypes.CLOSE_CIRCLE_OUTLINED} />
+          </span>
+          <h1 className="text-[24px] sm:text-[40px] font-bold text-center sm:leading-[45px]">
+            Время на оплату счета истекло
+          </h1>
+          <p className="sm:text-xl text-center">
+            Запросите новую ссылку для оплаты
+          </p>
+        </div>
+      </Modal>
+    </ModalWrapper>
+  );
+};
 
 export default ClosePayment;
--- src/features/badge/dangerPayment.tsx
+++ src/features/badge/dangerPayment.tsx
@@ -1,25 +1,56 @@
+import { paymentSelectors } from "@/entities/payment";
+import { useAction, useAppSelector } from "@/shared";
+import {
+  Button,
+  ButtonColors,
+  ButtonRoundSizes,
+} from "@/shared/ui/button/button";
 import { IconTypes } from "@/shared/ui/icon/libs/libs";
 import { Icon } from "@/shared/ui/icon/ui/icon";
 import Modal from "@/shared/ui/modal/modal";
 import ModalWrapper from "@/widgets/modalWrapper/ui/modalWrapper";
 
-const DangerPayment = () => (
-  <ModalWrapper className="-translate-y-52">
-    <Modal isOpen>
-      <div className="text-white bg-dark-200 rounded-xl flex justify-center flex-col items-center space-y-3 p-5 py-8 sm:w-[479px]">
-        <span>
-          <Icon type={IconTypes.DANGER_CIRCLE_OUTLINED} />
-        </span>
-        <h1 className="text-[24px] sm:text-[40px] font-bold">
-          Ссылка не найдена
-        </h1>
-        <p className="sm:text-xl text-center">
-          Возможно, она истекла или её вовсе не существовало. Получите новую в
-          магазине
-        </p>
-      </div>
-    </Modal>
-  </ModalWrapper>
-);
+const DangerPayment = () => {
+  const payment = useAppSelector(paymentSelectors.paymentParams);
+  const { clearPaymentInfo } = useAction();
+
+  const handleClear = () => {
+    clearPaymentInfo();
+    if (payment?.shop_tag) {
+      window.open(payment?.shop_tag ?? "", "_self");
+    }
+  };
+
+  return (
+    <ModalWrapper className="-translate-y-52">
+      <Modal isOpen>
+        <div className="text-white bg-dark-200 rounded-xl flex justify-center flex-col items-center space-y-3 p-5 py-8 sm:w-[479px]">
+          <span>
+            <Icon type={IconTypes.DANGER_CIRCLE_OUTLINED} />
+          </span>
+          <h1 className="text-[24px] sm:text-[40px] font-bold">
+            Ссылка не найдена
+          </h1>
+          <p className="sm:text-xl text-center">
+            Возможно, она истекла или её вовсе не существовало. Получите новую в
+            магазине
+          </p>
+          {payment?.shop_tag && (
+            <div className="flex justify-center w-full">
+              <Button
+                className="border p-2 "
+                rounded={ButtonRoundSizes.ROUNDED_2XL}
+                bgColor={ButtonColors.TIFFANY}
+                onClick={handleClear}
+              >
+                Вернуться в магазин
+              </Button>
+            </div>
+          )}
+        </div>
+      </Modal>
+    </ModalWrapper>
+  );
+};
 
 export default DangerPayment;
